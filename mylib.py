85d6921ef92601d6e25beb1ff3c12af4ce61bec5
Vybekid <felobitto@gmail.com>
2025-07-02 01:07:28 +0300

    wile loops

--- mylib.py
+++ mylib.py
@@ -1,1 +1,10 @@
-while True: print(eval(input("Write your expression: ")))
+
+while True: 
+    new_numbers = input("Enter the number: ")
+    new_number = int(new_numbers)
+    
+    if new_number >= 7:
+        print("I like the number")
+        break
+    else: 
+        print("I dont like the number, enter a new one")
